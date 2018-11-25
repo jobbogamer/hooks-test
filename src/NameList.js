7b35a1fe77f51da0e9d771c2e8ffc31518c5dcc1
Josh Asch <josh.asch@gmail.com>
2018-11-25 18:34:54 +0000

    Remove fake response stuff

--- src/NameList.js
+++ src/NameList.js
@@ -3,20 +3,6 @@ import axios from 'axios';
 
 const API_URL = 'https://uinames.com/api/';
 
-const fakeResponse = [
-  { name: 'Jim', surname: 'Pickens', gender: 'male', region: 'USA' },
-  { name: 'Simon', surname: 'Cowell', gender: 'male', region: 'UK' },
-  { name: 'Thomas', surname: 'Edison', gender: 'male', region: 'USA' },
-  { name: 'Vladimir', surname: 'Putin', gender: 'male', region: 'Russia' },
-  { name: 'Angela', surname: 'Murkel', gender: 'female', region: 'Germany' }
-];
-
-const shuffleArray = arr =>
-  arr
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1]);
-
 const NameList = ({ amount, region }) => {
   const [loaded, setLoaded] = useState(false);
   const [names, setNames] = useState([]);
